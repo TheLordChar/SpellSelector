0cf9824ad02da874478f440234999ad907cfdc5e
TheLordChar <70201346+TheLordChar@users.noreply.github.com>
2022-12-02 01:04:54 -0600

    Update Spell.java

--- src/Spell.java
+++ src/Spell.java
@@ -1,172 +1,39 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 public class Spell {
-    private String level;
-    private String name;
-    private String school;
-    private String ritual;
-    private String castingTime;
-    private String range;
-    private String spellSize;
-    private String shape;
-    private String verbal;
-    private String somatic;
-    private String material;
-    private String value;
-    private String consumed;
-    private String concentration;
-    private String duration;
-    private String higherLevels;
-    private String higherEffect;
-    private String attack;
-    private String save;
-    private String suck;
-    private String check;
-    private String dice;
-    private String dmgType;
-    private String effect;
-
-
-    public String getLevel(){
-        return this.level;
+    public
+        String CastingTime, Name, School, Shape, Duration, HigherLevels, HigherEffect, Attack, Save, Check, Dice, DmgType, Effect, Range, Size,Value;
+        boolean Ritual,Somatic, Verbal, Material,Consumed,Concentration, OrSuck;
+        int Level;
+    public Spell(JSONObject obj){
+        JSONObject o = (JSONObject) obj;
+        Level = Integer.parseInt((String)o.get("Level"));
+        Name = (String) o.get("Name");
+        School = (String) o.get("School");
+        Ritual = (((o.get("Ritual"))).equals("Yes"));
+        CastingTime = (String) o.get("Casting Time");
+        Range = (String)o.get("Range");
+        Size = (String) o.get("Size");
+        Shape = (String) o.get("Shape");
+        Verbal = (((o.get("Verbal"))).equals("Yes"));
+        Somatic = (((o.get("Somatic"))).equals("Yes"));
+        Material = (((o.get("Material"))).equals("Yes"));
+        Value = (String) o.get("Value");
+        Consumed = (((o.get("Consumed"))).equals("Yes"));
+        Concentration = (((o.get("Concentration"))).equals("Yes"));
+        Duration = (String) o.get("Duration");
+        HigherLevels = (String) o.get("Higher Levels");
+        HigherEffect = (String)o.get("Higher Effect");
+        Attack = (String) o.get("Attack");
+        Save = (String) o.get("Save...");
+        OrSuck = (((o.get("or Suck"))).equals("Yes"));
+        Check = (String) o.get("Check");
+        Dice = (String) o.get("Dice");
+        DmgType = (String) o.get("Dmg Type");
+        Effect = (String) o.get("Effect");
     }
-    public void setLevel(String level){
-        this.level = level;
-    }
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public String getSchool(){
-        return this.school;
-    }
-    public void setSchool(String school){
-        this.school = school;
-    }
-    public String getRitual(){
-        return this.ritual;
-    }
-    public void setRitual(String ritual){
-        this.ritual = ritual;
-    }
-    public String getCastingTime(){
-        return this.castingTime;
-    }
-    public void setCastingTime(String castingTime){
-        this.castingTime = castingTime;
-    }
-    public String getRange(){
-        return this.range;
-    }
-    public void setRange(String range){
-        this.range = range;
-    }
-    public String getSpellSize(){
-        return this.spellSize;
-    }
-    public void setSpellSize(String spellSize){
-        this.spellSize = spellSize;
-    }
-    public String getShape(){
-        return this.shape;
-    }
-    public void setShape(String shape){
-        this.shape = shape;
-    }
-    public String getVerbal(){
-        return this.verbal;
-    }
-    public void setVerbal(String verbal){
-        this.verbal = verbal;
-    }
-    public String getSomatic(){
-        return this.somatic;
-    }
-    public void setSomatic(String somatic){
-        this.somatic = somatic;
-    }
-    public String getMaterial(){
-        return this.material;
-    }
-    public void setMaterial(String material){
-        this.material = material;
-    }
-    public String getValue(){
-        return this.value;
-    }
-    public void setValue(String value){
-        this.value = value;
-    }
-    public String getConsumed(){
-        return this.consumed;
-    }
-    public void setConsumed(String consumed){
-        this.consumed = consumed;
-    }
-    public String getConcentration(){
-        return this.concentration;
-    }
-    public void setConcentration(String concentration){
-        this.concentration = concentration;
-    }
-    public String getDuration(){
-        return this.duration;
-    }
-    public void setDuration(String duration){
-        this.duration = duration;
-    }
-    public String getHigherLevels(){
-        return this.higherLevels;
-    }
-    public void setHigherLevels(String higherLevels){
-        this.higherLevels = higherLevels;
-    }
-    public String getHigherEffect(){
-        return this.higherEffect;
-    }
-    public void setHigherEffect(String higherEffect){
-        this.higherEffect = higherEffect;
-    }
-    public String getAttack(){
-        return this.attack;
-    }
-    public void setAttack(String attack){
-        this.attack = attack;
-    }
-    public String getSave(){
-        return this.save;
-    }
-    public void setSave(String save){
-        this.save = save;
-    }
-    public String getSuck(){
-        return this.suck;
-    }
-    public void setSuck(String suck){
-        this.suck = suck;
-    }
-    public String getCheck(){
-        return this.check;
-    }
-    public void setCheck(String check){
-        this.check = check;
-    }
-    public String getDice(){
-        return this.dice;
-    }
-    public void setDice(String dice){
-        this.dice = dice;
-    }
-    public String getDmgType(){
-        return this.dmgType;
-    }
-    public void setDmgType(String dmgType){
-        this.dmgType = dmgType;
-    }
-    public String getEffect(){
-        return this.effect;
-    }
-    public void setEffect(String effect){
-        this.effect = effect;
+    @Override
+    public String toString(){
+        return Name;
     }
 }
